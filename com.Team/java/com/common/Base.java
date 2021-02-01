3b0ae4b1effe584f09b940c1e3d1bb0f9391292f
ankurghose1 <78221633+ankurghose1@users.noreply.github.com>
2021-01-31 20:03:27 -0500

    Add files via upload

---
+++ com.Team/java/com/common/Base.java
@@ -0,0 +1,26 @@
+package com.common;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+	
+	public WebDriver driver;
+	
+	public WebDriver getdriver() {
+		
+		System.setProperty("webdriver.chrome.driver", "./Drivers/Chromedriver/chromedriver");
+		
+		driver = new ChromeDriver();
+		driver.get("http://chegg-qa-challenge.herokuapp.com/login");
+	//	driver.get("https://www.amazon.com/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		return driver;
+		//12
+		
+	}
+
+}
